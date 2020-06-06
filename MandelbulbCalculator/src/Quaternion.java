d17be8893f179b6170ac29d131a14799fb244d0c
jan <jan@172.16.1.112>
2020-06-06 18:39:31 +0200

    First commit

---
+++ MandelbulbCalculator/src/Quaternion.java
@@ -0,0 +1,45 @@
+public class Quaternion {
+
+	private Double x0;
+	private Double x1;
+	private Double x2;
+	private Double x3;
+	
+	public Quaternion (Double x0, Double x1, Double x2, Double x3) {
+		this.x0 = x0;
+		this.x1 = x1;
+		this.x2 = x2;
+		this.x3 = x3;
+	}
+	
+	public Quaternion add (Quaternion b) {
+		Quaternion a = this;
+		Double x0 = a.x0 + b.x0;
+		Double x1 = a.x1 + b.x1;
+		Double x2 = a.x2 + b.x2;
+		Double x3 = a.x3 + b.x3;
+		return new Quaternion (x0, x1, x2, x3);
+	}
+	
+	public Quaternion times (Quaternion b) {
+		Quaternion a = this;
+		Double x0 = a.x0 * b.x0 - a.x1 * b.x1 - a.x2 * b.x2 - a.x3 * b.x3;
+		Double x1 = a.x0 * b.x1 + a.x1 * b.x0 + a.x2 * b.x3 - a.x3 * b.x2;
+		Double x2 = a.x0 * b.x2 - a.x1 * b.x3 + a.x2 * b.x0 + a.x3 * b.x1;
+		Double x3 = a.x0 * b.x3 + a.x1 * b.x2 - a.x2 * b.x1 + a.x3 * b.x0;
+		return new Quaternion(x0, x1, x2, x3);
+	}
+	
+	public void printQuaternion() {
+		System.out.println(x0 + " + " + x1 + "i + " + x2 + "j + " + x3 + "k");
+	}
+	
+	public static void main(String[] args) {
+		Quaternion test = new Quaternion(1.00, 2.00, 3.00, 4.00);
+		Quaternion b = new Quaternion(4.00, 3.00, 2.00, 1.00);
+		test.times(b).printQuaternion();
+		test.printQuaternion();
+
+	}
+
+}
